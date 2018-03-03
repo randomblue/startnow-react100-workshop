79bb2d0fdc980238a366e4a7b7e09d2c7e4abdf7
randomblue <jpmedallon@gmail.com>
2018-03-02 19:11:22 -0800

    added on click alert

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
